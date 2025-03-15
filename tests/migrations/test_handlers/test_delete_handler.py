10438b417c3d7aa76a94155ccdad9538fc160b26
Vitalij Burkasov <burkasovvitalij@gmail.com>
2025-03-15 22:19:50 +0600

    login logic and tests

--- tests/migrations/test_handlers/test_delete_handler.py
+++ tests/migrations/test_handlers/test_delete_handler.py
@@ -1,5 +1,8 @@
 from uuid import uuid4
 
+from tests.conftest import create_test_auth_headers_for_user
+from tests.conftest import create_user_in_database
+
 
 async def test_delete_user(client, create_user_in_database, get_user_from_database):
     user_data = {
@@ -11,7 +14,10 @@ async def test_delete_user(client, create_user_in_database, get_user_from_databa
         "hashed_password": "string",
     }
     await create_user_in_database(**user_data)
-    resp = client.delete(f"/user/?user_id={user_data["user_id"]}")
+    resp = client.delete(
+        f"/user/?user_id={user_data["user_id"]}",
+        headers=create_test_auth_headers_for_user(user_data["email"]),
+    )
     assert resp.status_code == 200
     data_from_resp = resp.json()
     assert data_from_resp == {"deleted_user_id": str(user_data["user_id"])}
@@ -24,16 +30,101 @@ async def test_delete_user(client, create_user_in_database, get_user_from_databa
     assert user_from_db["user_id"] == user_data["user_id"]
 
 
-async def test_delete_user_not_found(client):
+async def test_delete_user_not_found(client, create_user_in_database):
+    user_data = {
+        "user_id": uuid4(),
+        "name": "Admin",
+        "surname": "Adminov",
+        "email": "admin@kek.com",
+        "is_active": True,
+        "hashed_password": "string",
+    }
+    await create_user_in_database(**user_data)
     user_id = uuid4()
-    resp = client.delete(f"/user/?user_id={user_id}")
+    resp = client.delete(
+        f"/user/?user_id={user_id}",
+        headers=create_test_auth_headers_for_user(user_data["email"]),
+    )
     assert resp.status_code == 404
     data_from_resp = resp.json()
     assert data_from_resp == {"detail": f"User with id {user_id} not found"}
 
 
-async def test_delete_user_id_validation_error(client):
-    resp = client.delete(f"/user/?user_id=123")
+async def test_delete_user_bad_creds(client, create_user_in_database):
+    user_data = {
+        "user_id": uuid4(),
+        "name": "Admin",
+        "surname": "Adminov",
+        "email": "admin@kek.com",
+        "is_active": True,
+        "hashed_password": "string",
+    }
+    await create_user_in_database(**user_data)
+    user_id = uuid4()
+    resp = client.delete(
+        f"/user/?user_id={user_id}",
+        headers=create_test_auth_headers_for_user(user_data["email"] + "a"),
+    )
+    assert resp.status_code == 401
+    data_from_resp = resp.json()
+    assert data_from_resp == {"detail": "Could not validate credentials"}
+
+
+async def test_delete_user_unauth(client, create_user_in_database):
+    user_data = {
+        "user_id": uuid4(),
+        "name": "Admin",
+        "surname": "Adminov",
+        "email": "admin@kek.com",
+        "is_active": True,
+        "hashed_password": "string",
+    }
+    await create_user_in_database(**user_data)
+    user_id = uuid4()
+    bad_auth_headers = create_test_auth_headers_for_user(user_data["email"])
+    bad_auth_headers["Authorization"] += "a"
+    resp = client.delete(
+        f"/user/?user_id={user_id}",
+        headers=bad_auth_headers,
+    )
+    assert resp.status_code == 401
+    data_from_resp = resp.json()
+    assert data_from_resp == {"detail": "Could not validate credentials"}
+
+
+async def test_delete_user_no_token(client, create_user_in_database):
+    user_data = {
+        "user_id": uuid4(),
+        "name": "Admin",
+        "surname": "Adminov",
+        "email": "admin@kek.com",
+        "is_active": True,
+        "hashed_password": "string",
+    }
+    await create_user_in_database(**user_data)
+    user_id = uuid4()
+    resp = client.delete(
+        f"/user/?user_id={user_id}",
+    )
+    assert resp.status_code == 401
+    data_from_resp = resp.json()
+    assert data_from_resp == {"detail": "Not authenticated"}
+
+
+async def test_delete_user_id_validation_error(client, create_user_in_database):
+    user_data = {
+        "user_id": uuid4(),
+        "name": "Admin",
+        "surname": "Adminov",
+        "email": "admin@kek.com",
+        "is_active": True,
+        "hashed_password": "string",
+    }
+    await create_user_in_database(**user_data)
+    resp = client.delete(
+        f"/user/?user_id=123",
+        headers=create_test_auth_headers_for_user(user_data["email"]),
+    )
     assert resp.status_code == 422
     data_from_resp = resp.json()
     assert data_from_resp == {
--- tests/migrations/test_handlers/test_get_handler.py
+++ tests/migrations/test_handlers/test_get_handler.py
@@ -1,5 +1,7 @@
 from uuid import uuid4
 
+from tests.conftest import create_test_auth_headers_for_user
+
 
 async def test_get_user(client, create_user_in_database):
     user_data = {
@@ -11,7 +13,10 @@ async def test_get_user(client, create_user_in_database):
         "hashed_password": "string",
     }
     await create_user_in_database(**user_data)
-    resp = client.get(f"/user/?user_id={user_data["user_id"]}")
+    resp = client.get(
+        f"/user/?user_id={user_data["user_id"]}",
+        headers=create_test_auth_headers_for_user(user_data["email"]),
+    )
     assert resp.status_code == 200
     data_from_resp = resp.json()
     assert data_from_resp["user_id"] == str(user_data["user_id"])
@@ -31,7 +36,10 @@ async def test_get_user_id_validation_error(client, create_user_in_database):
         "hashed_password": "string",
     }
     await create_user_in_database(**user_data)
-    resp = client.get(f"/user/?user_id=123")
+    resp = client.get(
+        f"/user/?user_id=123",
+        headers=create_test_auth_headers_for_user(user_data["email"]),
+    )
     assert resp.status_code == 422
     data_from_resp = resp.json()
     assert data_from_resp == {
@@ -60,7 +68,71 @@ async def test_get_user_not_found(client, create_user_in_database):
     }
     await create_user_in_database(**user_data)
     user_id_for_finding = uuid4()
-    resp = client.get(f"/user/?user_id={user_id_for_finding}")
+    resp = client.get(
+        f"/user/?user_id={user_id_for_finding}",
+        headers=create_test_auth_headers_for_user(user_data["email"]),
+    )
     assert resp.status_code == 404
     data_from_resp = resp.json()
     assert data_from_resp == {"detail": f"User with id {user_id_for_finding} not found"}
+
+
+async def test_get_user_bad_creds(client, create_user_in_database):
+    user_data = {
+        "user_id": uuid4(),
+        "name": "Admin",
+        "surname": "Adminov",
+        "email": "admin@kek.com",
+        "is_active": True,
+        "hashed_password": "string",
+    }
+    await create_user_in_database(**user_data)
+    user_id = uuid4()
+    resp = client.get(
+        f"/user/?user_id={user_id}",
+        headers=create_test_auth_headers_for_user(user_data["email"] + "a"),
+    )
+    assert resp.status_code == 401
+    data_from_resp = resp.json()
+    assert data_from_resp == {"detail": "Could not validate credentials"}
+
+
+async def test_get_user_unauth(client, create_user_in_database):
+    user_data = {
+        "user_id": uuid4(),
+        "name": "Admin",
+        "surname": "Adminov",
+        "email": "admin@kek.com",
+        "is_active": True,
+        "hashed_password": "string",
+    }
+    await create_user_in_database(**user_data)
+    user_id = uuid4()
+    bad_auth_headers = create_test_auth_headers_for_user(user_data["email"])
+    bad_auth_headers["Authorization"] += "a"
+    resp = client.get(
+        f"/user/?user_id={user_id}",
+        headers=bad_auth_headers,
+    )
+    assert resp.status_code == 401
+    data_from_resp = resp.json()
+    assert data_from_resp == {"detail": "Could not validate credentials"}
+
+
+async def test_get_user_no_token(client, create_user_in_database):
+    user_data = {
+        "user_id": uuid4(),
+        "name": "Admin",
+        "surname": "Adminov",
+        "email": "admin@kek.com",
+        "is_active": True,
+        "hashed_password": "string",
+    }
+    await create_user_in_database(**user_data)
+    user_id = uuid4()
+    resp = client.get(
+        f"/user/?user_id={user_id}",
+    )
+    assert resp.status_code == 401
+    data_from_resp = resp.json()
+    assert data_from_resp == {"detail": "Not authenticated"}
