aea574c4082c45639e8bafd49c4531b5c720804d
Vitalij <153791011+Vitalij-Burk@users.noreply.github.com>
2025-03-19 18:43:45 +0600

    Add role management (#5)
    * roles logic
    
    * add role management

--- tests/migrations/test_handlers/test_delete_handler.py
+++ tests/migrations/test_handlers/test_delete_handler.py
@@ -168,7 +168,7 @@ async def test_delete_user_by_privilege_roles(
         "email": "admin@kek.com",
         "is_active": True,
         "hashed_password": "string",
-        "roles": ["ROLE_PORTAL_USER"],
+        "roles": [PortalRole.ROLE_PORTAL_USER],
     }
     user_data = {
         "user_id": uuid4(),
@@ -278,3 +278,33 @@ async def test_delete_another_user_error(
         headers=create_test_auth_headers_for_user(user_who_delete["email"]),
     )
     assert resp.status_code == 403
+
+
+async def test_reject_delete_superadmin(
+    client, create_user_in_database, get_user_from_database
+):
+    user_data = {
+        "user_id": uuid4(),
+        "name": "Admin",
+        "surname": "Adminov",
+        "email": "admin@kek.com",
+        "is_active": True,
+        "hashed_password": "string",
+        "roles": [PortalRole.ROLE_PORTAL_SUPERADMIN],
+    }
+    await create_user_in_database(**user_data)
+    resp = client.delete(
+        f"/user/?user_id={user_data["user_id"]}",
+        headers=create_test_auth_headers_for_user(user_data["email"]),
+    )
+    assert resp.status_code == 406
+    data_from_resp = resp.json()
+    assert data_from_resp == {"detail": "Superadmin cannot be deleted via API"}
+    users_from_db = await get_user_from_database(user_data["user_id"])
+    user_from_db = dict(users_from_db[0])
+    assert user_from_db["name"] == user_data["name"]
+    assert user_from_db["surname"] == user_data["surname"]
+    assert user_from_db["email"] == user_data["email"]
+    assert user_from_db["is_active"] is True
+    assert user_from_db["user_id"] == user_data["user_id"]
+    assert PortalRole.ROLE_PORTAL_SUPERADMIN in user_from_db["roles"]
